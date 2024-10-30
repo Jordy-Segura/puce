6b707598fb4ee01bfde2976897cec6c05e765ced
Charly-004 <charlycortes004@gmail.com>
2024-10-30 15:16:15 -0500

    Bienvenidos a Puce :)

---
+++ src/main/java/ec/edu/espoch/puce/Puce.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.puce;
+
+/**
+ *
+ * @author jordy
+ */
+public class Puce {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos a la PUCE");
+    }
+}
